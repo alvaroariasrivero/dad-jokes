25ccf5ee7663377f357b25ef43d3548b4b65d455
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-09-13 20:22:08 +0200

    script using async await

--- script.js
+++ script.js
@@ -4,35 +4,35 @@ const jokeBtn = document.getElementById('joke-btn');
 
 //Using async await
 
-// const generateJoke = async () => {
-//     const config = {
-//         headers: {
-//           Accept: 'application/json',
-//         },
-//     }
-
-//     const res = await fetch('https://icanhazdadjoke.com', config);
-
-//     const data = await res.json();
-
-//     jokeEl.innerText = data.joke;
-// }
-
-//Using .then()
-
-const generateJoke = () => {
+const generateJoke = async () => {
     const config = {
         headers: {
           Accept: 'application/json',
         },
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then((res) => res.json())
-        .then((data) => {
-            jokeEl.innerText = data.joke;
-        });
-};
+    const res = await fetch('https://icanhazdadjoke.com', config);
+
+    const data = await res.json();
+
+    jokeEl.innerText = data.joke;
+}
+
+//Using .then()
+
+// const generateJoke = () => {
+//     const config = {
+//         headers: {
+//           Accept: 'application/json',
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerText = data.joke;
+//         });
+// };
 
 jokeBtn.addEventListener('click', generateJoke);
 
